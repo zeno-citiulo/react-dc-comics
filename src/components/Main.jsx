82572af437511fe75a3b8fc885f3167f6ad77f80
zeno-citiulo <zenocit02@gmail.com>
2025-01-18 14:52:56 +0100

    feat: main completed almost

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,32 @@
 const Main = () => {
   return (
     <main>
+      <section className="dc-banner dc-banner--black">
+        <h2>--&gt; Content goes here &lt;--</h2>
+      </section>
 
+      <section className="dc-banner dc-banner--blue">
+        <div className="dc-banner__item">
+          <span className="icon icon-digital-comics" />
+          <p>Digital Comics</p>
+        </div>
+        <div className="dc-banner__item">
+          <span className="icon icon-merchandise" />
+          <p>DC Merchandise</p>
+        </div>
+        <div className="dc-banner__item">
+          <span className="icon icon-subscription" />
+          <p>Subscription</p>
+        </div>
+        <div className="dc-banner__item">
+          <span className="icon icon-shop-locator" />
+          <p>Comic Shop Locator</p>
+        </div>
+        <div className="dc-banner__item">
+          <span className="icon icon-power-visa" />
+          <p>DC Power Visa</p>
+        </div>
+      </section>
     </main>
   )
 }
